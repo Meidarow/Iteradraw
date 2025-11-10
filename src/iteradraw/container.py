25e4b398eb73bf9fa2f8f911d0361df90c0b25c3
Meidarow <60526952+Meidarow@users.noreply.github.com>
2025-11-10 00:14:40 -0300

    feat: DI container now splits installation into smaller classes/modules

--- src/iteradraw/container.py
+++ src/iteradraw/container.py
@@ -23,19 +23,44 @@ from iteradraw.infrastructure.persistence.sqlite3_domain_database import (
 )
 
 
-class CommandAssignments:
-    def __init__(self): ...
+class Container:
+    # TODO if methods stay small -> unify them in build
+    def __init__(self):
+        self.container = None
 
-    def _instantiate_shared_objects(self):
-        self.event_bus = EventBus()
-        self.command_bus = CommandBus()
-        self.folder_repo = FolderRepository(
-            persistence=SQLite3DomainDatabase(db_path="")
-        )
+    def build(self):
+        self._build_container()
+        self._install_modules()
+
+    def _build_container(self):
+        self.container = {
+            "event_bus": EventBus(),
+            "command_bus": CommandBus(),
+            "folder_repo": FolderRepository(
+                persistence=SQLite3DomainDatabase(db_path="")
+            ),
+        }
+
+    def _install_modules(self):
+        CommandAssignment().register(self.container)
+
+
+class CommandAssignment:
+    def __init__(self):
+        self.folder_repo = None
+        self.command_bus = None
+        self.event_bus = None
+
+    def register(self, container):
+        self.event_bus = container["event_bus"]
+        self.command_bus = container["command_bus"]
+        self.folder_repo = container["folder_repo"]
+        self._assign_folder_command_handlers()
+        self._assign_timer_command_handlers()
 
     def _assign_folder_command_handlers(self):
         add_folder_handler = AddFolderCommandHandler(
-            folder_repo=self.folder_repo
+            folder_repo=self.folder_repo, event_bus=self.event_bus
         )
         self.command_bus.register(
             command_type=AddFolderCommand,
@@ -43,7 +68,7 @@ class CommandAssignments:
         )
 
         remove_folder_handler = RemoveFolderCommandHandler(
-            folder_repo=self.folder_repo
+            folder_repo=self.folder_repo, event_bus=self.event_bus
         )
         self.command_bus.register(
             command_type=RemoveFolderCommand,
@@ -51,7 +76,7 @@ class CommandAssignments:
         )
 
         rename_folderset_handler = RenameFolderSetCommandHandler(
-            folder_repo=self.folder_repo
+            folder_repo=self.folder_repo, event_bus=self.event_bus
         )
         self.command_bus.register(
             command_type=RenameFolderSetCommand,
@@ -59,7 +84,7 @@ class CommandAssignments:
         )
 
         add_folderset_handler = AddFolderSetCommandHandler(
-            folder_repo=self.folder_repo
+            folder_repo=self.folder_repo, event_bus=self.event_bus
         )
         self.command_bus.register(
             command_type=AddFolderSetCommand,
@@ -67,7 +92,7 @@ class CommandAssignments:
         )
 
         remove_folderset_handler = RemoveFolderSetCommandHandler(
-            folder_repo=self.folder_repo
+            folder_repo=self.folder_repo, event_bus=self.event_bus
         )
         self.command_bus.register(
             command_type=RemoveFolderSetCommand,
