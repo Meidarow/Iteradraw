68844d9b62dc3ca3ce53f26980a0c81b53dc4e0d
Meidarow <60526952+Meidarow@users.noreply.github.com>
2026-03-10 15:23:13 -0300

    feature: implemented all folderset database operations and separated directory repo

--- src/iteradraw/core/domain/repositories/folder_repository.py
+++ src/iteradraw/core/domain/repositories/folder_repository.py
@@ -1,47 +1,13 @@
 import sqlite3
 from pathlib import Path
 
-from iteradraw.core.domain.exceptions import PersistenceError, CommitError
+from iteradraw.core.domain.exceptions import PersistenceError
 from iteradraw.core.domain.models.folder import FolderSet, Folder
 from iteradraw.core.infrastructure.persistence.sqlite3_database import SQLite3Database
-from iteradraw.interfaces import FolderRepository
+from iteradraw.interfaces import FolderRepository, DirectoryRepository
 
 
-class SQLFolderRepository(FolderRepository):
-
-    DB_SCHEMA = """
-    CREATE TABLE IF NOT EXISTS foldersets (
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL
-    );
-        
-    CREATE TABLE IF NOT EXISTS directories (
-        dir_id INTEGER NOT NULL PRIMARY KEY,
-        dir_name TEXT NOT NULL,
-        crawl_time INT NOT NULL,
-        mod_time INT NOT NULL, 
-    );
-        
-    CREATE TABLE IF NOT EXISTS rootfolders (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        path TEXT NOT NULL,
-        enabled BOOLEAN DEFAULT 1,
-        folderset_id INT NOT NULL,
-        UNIQUE (path, folderset_id),
-        FOREIGN KEY(folderset_id) REFERENCES foldersets(id)
-        ON DELETE CASCADE,
-        FOREIGN KEY (id) REFERENCES directories(dir_id)
-    );
-        
-    CREATE TABLE IF NOT EXISTS folders_closure (
-        ancestor_id INT NOT NULL,
-        descendant_id INT NOT NULL,
-        depth INT NOT NULL,
-        PRIMARY KEY (ancestor_id, descendant_id),
-        FOREIGN KEY (ancestor_id) REFERENCES directories(dir_id),
-        FOREIGN KEY (descendant_id) REFERENCES directories(dir_id)
-    );
-    """
+class SQLFoldersetRepository(FolderRepository):
 
     def __init__(
         self,
@@ -49,17 +15,7 @@ class SQLFolderRepository(FolderRepository):
     ) -> None:
         self.database = database
 
-    def initialize(self) -> None:
-        """Configure database and ensure the schema exists."""
-        self.database.open()
-        self.database.execute("PRAGMA journal_mode = WAL;")
-        self.database.execute("PRAGMA synchronous = NORMAL;")
-        self.database.execute("PRAGMA temp_store = MEMORY;")
-        self.database.execute("PRAGMA foreign_keys = ON;")
-        self.database.executescript(self.DB_SCHEMA)
-        self.database.row_factory = sqlite3.Row
-
-    def get_all_foldersets(self) -> list[FolderSet]:
+    def get_foldersets(self) -> list[FolderSet]:
         """
         Retrieves all FolderSets from the DB and reconstructs them.
         """
@@ -87,29 +43,33 @@ class SQLFolderRepository(FolderRepository):
                 folders=data["folders"],
             ) for folderset_id, data in foldersets_map.items()]
 
-    def save_folderset(self, folderset: FolderSet):
+    def update_folderset(self, folderset: FolderSet) -> None:
         """
-        Saves a FolderSet aggregate to the DB in a transaction.
+        Updates a FolderSet aggregate in the DB in a transaction.
         """
         try:
             with self.database:
-                self._upsert_foldersets(folderset)
+                self._upsert_folderset(folderset)
                 root_folder_list = [str(folder) for folder in folderset.folders.keys()]
-                inserted_directory_id_list = self._insert_directories(root_folder_list)
-                self._insert_closure_nodes(inserted_directory_id_list)
+                inserted_directory_id_list = self._insert_root_directories(root_folder_list)
+                self._insert_reflective_closure_nodes(inserted_directory_id_list)
                 folderset_data =[(str(f.path), f.enabled, folderset.id) for f in folderset.all]
                 self._insert_root_folders(folderset_data)
                 self._prune_root_folders(root_folder_list, folderset.id)
-                self._prune_directories()
+                self._prune_directories_without_root()
         except sqlite3.DatabaseError as e:
             raise PersistenceError("Failed to save folderset") from e
 
-    def register_folderset(self, folderset_name: str) -> int:
+    def add_folderset(self, folderset_name: str) -> int:
+        """
+        Adds a folderset to the DB.
+        Returns the unique id of the added folderset.
+        """
         try:
             with self.database:
                 cursor = self.database.execute(
                     """
-                    INSERT INTO foldersets VALUES (?) RETURNING id;
+                    INSERT INTO foldersets (name) VALUES (?) RETURNING id;
                     """,
                     (folderset_name,),
                 )
@@ -124,27 +84,13 @@ class SQLFolderRepository(FolderRepository):
                     "DELETE FROM foldersets WHERE id = ?",
                     (folderset_id,),
                 )
-                self._prune_directories()
+                self._prune_directories_without_root()
         except sqlite3.DatabaseError as e:
             raise PersistenceError("Failed to delete folderset") from e
 
-    ### Crawl relevant
-
-    def add_discovered_folder(self):
-        #todo
-        ...
-
-
-    def fetch_directories(self) -> list[Folder]:
-        query = """
-        SELECT dir_id from directories WHERE crawl_time < mod_time OR crawl_time = 0
-        """
-
-
     # Private Helpers:
 
-
-    def _insert_directories(self, root_folders: list[str]) ->list[int]:
+    def _insert_root_directories(self, root_folders: list[str]) ->list[int]:
         dir_id_list = []
         if root_folders:
             insert_folders_query = " ".join(
@@ -156,7 +102,7 @@ class SQLFolderRepository(FolderRepository):
             dir_id_list = [(directory_id["dir_id"]) for directory_id in rows]
         return dir_id_list
 
-    def _prune_directories(self) -> None:
+    def _prune_directories_without_root(self) -> None:
         self.database.execute(
             """
             DELETE FROM directories WHERE dir_id NOT IN (
@@ -165,7 +111,7 @@ class SQLFolderRepository(FolderRepository):
             """
         )
 
-    def _insert_closure_nodes(self, dir_id_list: list[int]) -> None:
+    def _insert_reflective_closure_nodes(self, dir_id_list: list[int]) -> None:
         if dir_id_list:
             directory_data = [(directory_id, directory_id) for directory_id in dir_id_list]
             self.database.executemany(
@@ -175,7 +121,7 @@ class SQLFolderRepository(FolderRepository):
                 """,
                 directory_data)
 
-    def _upsert_foldersets(self, folderset: FolderSet) -> None:
+    def _upsert_folderset(self, folderset: FolderSet) -> None:
         self.database.execute(
             """
             INSERT INTO foldersets (id, name) VALUES (?, ?)
@@ -202,4 +148,60 @@ class SQLFolderRepository(FolderRepository):
             " ".join([f"DELETE FROM rootfolders WHERE folderset_id = ? AND path NOT IN",
             "(", ", ".join(["?" for _ in range(len(root_folders))]), ")"]),
             params,
+        )
+
+
+class SQLDirectoryRepository(DirectoryRepository):
+
+    def __init__(
+        self,
+        database: SQLite3Database,
+    ) -> None:
+        self.database = database
+
+    def add_discovered_folder(self, dir_name: str, parent_id, crawl_time: int, mod_time: int) -> None:
+        new_dir_id = self._add_directory_node(dir_name, crawl_time, mod_time)
+        self._add_closure_edges(parent_id, new_dir_id)
+
+    def get_stale_directories(self) -> list[tuple[int, str]]:
+        query = """ 
+        SELECT dir_id, dir_name from directories 
+        WHERE crawl_time < mod_time OR crawl_time = 0
+        """
+        return [(directory["dir_id"], directory["name"]) for directory in self.database.execute(query).fetchall()]
+
+    def get_normalized_path(self, dir_id: int) -> Path:
+        query = """ 
+        SELECT dir_name 
+        FROM directories d 
+        INNER JOIN folders_closure c ON d.dir_id = c.ancestor_id 
+        WHERE c.descendant_id = ? 
+        ORDER BY c.depth DESC
+        """
+        cursor = self.database.execute(query, (dir_id,)).fetchall()
+        return Path(*[segment["dir_name"] for segment in cursor])
+
+    def update_dir(self, dir_id: int, crawl_time: int, mod_time: int) -> None:
+        query = """
+        UPDATE directories SET crawl_time = ?, mod_time = ? WHERE dir_id = ?
+        """
+        self.database.execute(query, (crawl_time, mod_time, dir_id))
+
+    def _add_directory_node(self, dir_name: str, crawl_time: int, mod_time: int) -> int:
+        query = """
+                INSERT INTO directories (dir_name, crawl_time, mod_time) 
+                VALUES (?, ?, ?) RETURNING dir_id; 
+                """
+        return self.database.execute(query, (dir_name, crawl_time, mod_time)).fetchone()["dir_id"]
+
+    def _add_closure_edges(self, parent_id: int, current_dir_id: int) -> None:
+        self.database.execute("""
+                              INSERT INTO folders_closure (ancestor_id, descendant_id, depth)
+                              SELECT c.ancestor_id, ?, c.depth + 1
+                              FROM folders_closure c
+                              WHERE descendant_id = ?
+                              """, [current_dir_id, parent_id])
+        self.database.execute(
+            "INSERT INTO folders_closure (ancestor_id, descendant_id, depth) VALUES (?, ?, 0)",
+            (current_dir_id, current_dir_id),
         )
