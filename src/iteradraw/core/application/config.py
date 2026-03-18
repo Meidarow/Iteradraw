92b1b935ea14b7dc2ebd24bd3e2a3a22e4f5a531
Meidarow <60526952+Meidarow@users.noreply.github.com>
2026-03-18 19:23:42 -0300

    feat: updated db path

--- src/iteradraw/core/application/config.py
+++ src/iteradraw/core/application/config.py
@@ -1,5 +1,5 @@
 
 class ApplicationConfiguration:
     def __init__(self):
-        self.db_path = ":memory:"
+        self.db_path = "/Users/gabriel/Desktop/tmp/database.db"
         self.crawler_batch_size = 5000
