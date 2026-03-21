ea4ee1be9b274b82d6bac92adf782f15129e5753
Meidarow <60526952+Meidarow@users.noreply.github.com>
2026-03-20 22:52:37 -0300

    docs: docstrings for sqlite3 database added.

--- src/iteradraw/core/infrastructure/persistence/sqlite3_database.py
+++ src/iteradraw/core/infrastructure/persistence/sqlite3_database.py
@@ -37,43 +37,93 @@ class SQLite3Database(Database):
         self.connection.__exit__(exc_type, exc_val, exc_tb)
 
     def open(self) -> None:
+        """
+        Initializes database connection.
+
+        SQLite does not allow for thread safety level (synchronous) to be
+        changed during a transaction. Since autocommit = False always keeps a
+        transaction open, here we briefly set autocommit = True, set thread
+        safety level, then switch back to False. Explicit transaction control
+        happens through commit() and rollback().
+        """
         if not self.connection:
-            self.connection = sqlite3.connect(self.db_path, autocommit=False)
+            self.connection = sqlite3.connect(
+                self.db_path,
+                autocommit=True,
+                check_same_thread=False
+            )
             self.connection.row_factory = sqlite3.Row
             script = """
-            COMMIT;
-            PRAGMA journal_mode = WAL;
             PRAGMA synchronous = NORMAL;
-            PRAGMA temp_storage = MEMORY;
+            PRAGMA journal_mode = WAL;
+            PRAGMA temp_store = MEMORY;
             PRAGMA foreign_keys = ON;
             """
             self.connection.executescript(script)
+            self.connection.autocommit = False
 
     def close(self) -> None:
+        """
+        Closes database connection.
+
+        Notes: This should only be done at application teardown, the same
+        connection can be reused in many transactions.
+        """
         if self.connection:
             self.connection.close()
+            self.connection = None
 
     def commit(self) -> None:
+        """
+        Commits changes.
+
+        Closes transaction persisting changes and opens a new one.
+        """
         if not self.connection:
             raise DatabaseConnectionError("Connection not open")
         self.connection.commit()
 
     def rollback(self) -> None:
+        """
+        Rolls back changes.
+
+        Closes transaction discarding changes and opens new one.
+        """
         if not self.connection:
             raise DatabaseConnectionError("Connection not open")
         self.connection.rollback()
 
     def execute(self, query: str, params: Sequence[Any] = ()) -> sqlite3.Cursor:
+        """
+        Executes single SQL statement.
+
+        Args:
+            query: SQL statement string.
+            params: Values plugged into slots (?) in statement.
+        """
         if not self.connection:
             raise DatabaseConnectionError("Connection not open")
         return self.connection.execute(query, params)
 
     def executemany(self, query: str, params: Sequence[Any] = ()) -> sqlite3.Cursor:
+        """
+        Executes a statement repeatedly in a single transaction.
+
+        Args:
+            query: SQL statement string.
+            params: Values plugged into slots (?) in statement.
+         """
         if not self.connection:
             raise DatabaseConnectionError("Connection not open")
         return self.connection.executemany(query, params)
 
     def executescript(self, script: str) -> sqlite3.Cursor:
+        """
+        Executes a series of statements.
+
+        Args:
+            script: SQL script string.
+        """
         if not self.connection:
             raise DatabaseConnectionError("Connection not open")
         return self.connection.executescript(script)
