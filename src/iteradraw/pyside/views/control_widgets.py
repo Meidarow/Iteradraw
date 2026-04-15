4b024a9569bc7995480b081aab58c1bd8e3aa67e
Meidarow <60526952+Meidarow@users.noreply.github.com>
2026-04-15 11:48:58 -0300

    refactor: SoC and organization

--- src/iteradraw/pyside/views/control_widgets.py
+++ src/iteradraw/pyside/views/control_widgets.py
@@ -7,15 +7,27 @@ from PySide6.QtWidgets import (
     QPushButton,
 )
 
+from iteradraw.pyside.pyside_shell import PySideShell
+
 
 class SidePanelView(QGroupBox):
-    def __init__(self):
+    def __init__(self, shell: PySideShell):
         super().__init__()
-        form = QFormLayout()
-        form.addRow("Timer (seconds):", QSpinBox())
-        form.addRow("Shuffle:", QCheckBox())
-        form.addRow(QPushButton("Start"))
-        self.setLayout(form)
+        self.shell = shell
+        self._form = None
+        self._build()
+
+    def _build(self):
+        self._form = QFormLayout()
+
+        self._timer_field = QSpinBox()
+        self._shuffle_checkbox = QCheckBox()
+        self._start_button = QPushButton("Start")
+
+        self._form.addRow("Timer (seconds):", self._timer_field)
+        self._form.addRow("Shuffle:", self._shuffle_checkbox)
+        self._form.addRow(self._start_button)
+        self.setLayout(self._form)
 
 
 class TimerSelectionView(QWidget):
